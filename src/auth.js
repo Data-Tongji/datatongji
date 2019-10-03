e10195517ef9380b621d9a51bf0b7c41cbe886e4
Leonardo Ronne <leoronne@gmail.com>
2019-10-02 21:58:04 -0300

    changes on package.json

--- src/auth.js
+++ src/auth.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import {Route, Redirect } from 'react-router-dom';
+import { Route, Redirect } from 'react-router-dom';
 
 function authenticate_token () {
   const requestInfo = {
